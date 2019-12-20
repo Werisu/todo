832cd62bde4814c81c862b663b29fa21c8fbbc4f
Werisu <wellysson35@gmail.com>
2019-12-20 17:50:39 -0300

    Lendo e Escrevendo Dados usando a função Future

--- lib/main.dart
+++ lib/main.dart
@@ -24,17 +24,30 @@ class _HomeState extends State<Home> {
     return Container();
   }
 
+  /**
+   * Future _getFile função assicrona
+   * pega o diretório onde irá ser armazenado os dados
+   */
   Future<File> _getFile() async{
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
   }
 
+  /**
+   * Future _save
+   * Função para salvar os dados
+   * retorna um arquivo
+   */
   Future<File> _saveData() async{
     String data = json.encode(_toDoList);
     final file = await _getFile();
     return file.writeAsString(data);
   }
 
+  /**
+   * Future _readData
+   * Função para ler os dados
+   */
   Future<String> _readData() async{
     try{
       final file = await _getFile();
