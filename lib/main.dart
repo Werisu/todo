7b832b31b5a69357c3ca54acc728295bdeabeab9
Werisu <wellysson35@gmail.com>
2019-12-30 19:16:01 -0300

    Remover pilha de SnacBar, removendo bug

--- lib/main.dart
+++ lib/main.dart
@@ -154,6 +154,7 @@ class _HomeState extends State<Home> {
             duration: Duration(seconds: 2),
           );
 
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
 
         });
